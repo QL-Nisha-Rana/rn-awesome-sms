4ea8974e87c2cfa05f776cb67c518be6dc10ca0a
saurabh upadhyay <saurabh.upadhyay@quokkalabs.com>
2024-05-22 17:14:44 +0530

    [FIX]: linting problems

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-awesome-sms' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+	`The package 'react-native-awesome-sms' doesn't seem to be linked. Make sure: \n\n` +
+	Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+	'- You rebuilt the app after installing the package\n' +
+	'- You are not using Expo Go\n';
 
 const AwesomeSms = NativeModules.AwesomeSms
-  ? NativeModules.AwesomeSms
-  : new Proxy(
-    {},
-    {
-      get() {
-        throw new Error(LINKING_ERROR);
-      },
-    }
-  );
+	? NativeModules.AwesomeSms
+	: new Proxy(
+		{},
+		{
+			get() {
+				throw new Error(LINKING_ERROR);
+			},
+		}
+	);
 
 export function getReceivedMessages() {
-  return AwesomeSms.getReceivedMessages();
+	return AwesomeSms.getReceivedMessages();
 }
 export function checkAndRequestPermissions() {
-  return AwesomeSms.checkAndRequestPermissions();
+	return AwesomeSms.checkAndRequestPermissions();
 }
